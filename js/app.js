c4af892985cd334a9643186221e222a463025880
Neiva <a.dcm33@gmail.com>
2021-10-13 18:58:20 +0200

    menu hiding when scroll down feature

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const hireMeBtn = document.querySelector('.hireme-btn-container');
-const about = document.querySelector('.about');
+const navBtn = document.querySelectorAll('.menu-li');
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.navlinks');
 const navBar = document.querySelector('nav');
+var width = window.innerWidth;
+
+console.log(width)
 
 const navSlide = () => {
 
@@ -12,11 +15,15 @@ const navSlide = () => {
             nav.classList.toggle('nav-active');
         });
     
-    about.addEventListener('click', function (){
-        if(nav.classList.contains('nav-active')){
-            nav.classList.remove('nav-active'); 
-        }
+    
+    navBtn.forEach(btn => {
+        btn.addEventListener('click', function (){
+            if(nav.classList.contains('nav-active')){
+                nav.classList.remove('nav-active'); 
+            }
+        });
     });
+
 
     window.addEventListener('scroll', () => {
         navBar.classList.toggle('sticky', window.scrollY > 0);
@@ -31,6 +38,40 @@ const navSlide = () => {
 
 navSlide();
 
+
+window.addEventListener('resize', function() {
+    width = window.innerWidth;
+    console.log(width);
+    return width;
+});
+
+
+function navBarHiding() {
+    if(width < 768){
+
+        var lastScrollTop = 0;
+        window.addEventListener('scroll', function(){
+            var scrollTop = window.pageYOffset || this.document.documentElement.scrollTop;
+            if (scrollTop > lastScrollTop){
+                navBar.style.top="-80px";
+                if(nav.classList.contains('nav-active')){
+                    nav.classList.remove('nav-active'); 
+                }
+            }
+                else{
+                    navBar.style.top="0";
+                }
+                lastScrollTop = scrollTop;
+        });
+
+    }
+}
+
+navBarHiding();  // bug ! le if (width < 768) ne marche pas.
+
+
+
+    
 
 
 /*
